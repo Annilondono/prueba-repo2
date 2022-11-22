2670fb7a10c5c89594e76feb30a20006dfe88e75
¨ana¨ <londonoanamilena@hotmail.com>
2022-11-22 17:50:26 -0300

    nuevos cambios

---
+++ javascript/index.js
@@ -0,0 +1,49 @@
+/* let nombre = "Ana";
+
+let numeroUno = 5;
+
+const numeroDos = 10;
+
+console.log(nombre)
+
+console.log(numeroDos) */
+
+
+let nombre = "Ana";
+let apellido = "Londono";
+let edad = 35;
+let mascota ="gato";
+
+let nombreApellido = nombre + " " + apellido;
+
+console.log(nombreApellido);
+
+let entrada = prompt("ingrese su nombre")
+let salida = entrada + " " + "ingresado";
+
+alert(entrada);
+alert(salida)
+
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(mascota);
+
+
+let numeroUno = 5;
+
+const numeroDos = 10;
+
+let resultadoSuma = numeroUno + numeroDos;
+
+let resultadoResta = numeroUno - numeroDos;
+
+let resultadoMulti = numeroUno * numeroDos;
+
+let resultadoDiv = numeroUno / numeroDos;
+
+console.log(resultadoDiv)
+
+
+
